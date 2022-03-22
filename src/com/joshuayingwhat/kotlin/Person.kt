3ab50c34d8ea7872931d2d0a3c8e437e0ac9eeaf
JoshuaYingWhatEgr <joshuayingwhat@163.com>
2022-03-22 22:03:31 +0800

    define class

---
+++ src/com/joshuayingwhat/kotlin/Person.kt
@@ -0,0 +1,19 @@
+package com.joshuayingwhat.kotlin
+
+fun main() {
+    var person = Person("joshua")
+
+    //setInfo(String info)
+    person.info = "hello"
+
+    //getInfo()
+    println(person.info)
+}
+
+class Person(_name: String) {
+    var info = "info =  $_name".also(::println)
+
+    init {
+        "info = $info".also(::println)
+    }
+}
